c1a40c082a7b8ca5dbc1d44935e83800f88bf918
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-02-15 18:11:24 -0300

    refatora HomeCubit para HomeController e simplifica a lógica de carregamento de contatos

--- lib/android/controller/home.controller.dart
+++ lib/android/controller/home.controller.dart
@@ -1,24 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:result_dart/result_dart.dart';
-import '../core/services/contact.service.dart';
-import '../models/contact.model.dart';
 
-class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial()) {
-    fetchContacts();
-  }
-
-  final ContactService _service = ContactService();
-
-  AsyncResult<List<Contact>> fetchContacts() async {
-    emit(HomeLoading());
-    final result = await _service.getContacts();
-    result.fold(
-      (contacts) => emit(HomeLoaded(contacts)),
-      (error) => emit(HomeError(error.toString())),
-    );
-    return result;
-  }
+class HomeController extends Cubit<HomeState> {
+  HomeController() : super(HomeInitial());
 }
 
 abstract class HomeState {}
@@ -27,11 +10,7 @@ class HomeInitial extends HomeState {}
 
 class HomeLoading extends HomeState {}
 
-class HomeLoaded extends HomeState {
-  HomeLoaded(this.contacts);
-
-  final List<Contact> contacts;
-}
+class HomeLoaded extends HomeState {}
 
 class HomeError extends HomeState {
   HomeError(this.message);
--- lib/android/view/home.view.dart
+++ lib/android/view/home.view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../controller/home.controller.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,12 +8,12 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => HomeCubit(),
+      create: (context) => HomeController(),
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          title: Text('Meus Contatos'),
+          title: Text('Home Page'),
           centerTitle: true,
           leading: IconButton(
             icon: Icon(Icons.search),
@@ -22,57 +21,7 @@ class HomeView extends StatelessWidget {
             onPressed: () {},
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocBuilder<HomeCubit, HomeState>(
-                builder: (context, state) {
-                  if (state is HomeLoading) {
-                    return CircularProgressIndicator();
-                  } else if (state is HomeLoaded) {
-                    return Expanded(
-                      child: SingleChildScrollView(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: state.contacts.length,
-                          itemBuilder: (context, index) {
-                            final contact = state.contacts[index];
-                            return ListTile(
-                              leading: CircleAvatar(child: Icon(Icons.person)),
-                              title: Text(
-                                contact.name,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              subtitle: Text(
-                                contact.email,
-                                style: TextStyle(fontSize: 12),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    );
-                  } else if (state is HomeError) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Erro: ${state.message}'),
-                    );
-                  } else {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Clique no botão para carregar os contatos'),
-                    );
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
+        body: Column(),
       ),
     );
   }
